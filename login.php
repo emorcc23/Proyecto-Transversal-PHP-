ecb4d7fc0608ebbf32440ea8af245b5570bca6ea
isainrono <>
2018-02-16 16:45:00 +0100

    He creado las piginas de Registro y login.

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Esta es la pagina para hacer login (usuarios registrados) .. Musicos, Locales, Fans
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ registro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Esta es la pagina para registrar usaurios. 
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
