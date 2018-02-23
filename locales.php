a3b0789ce89e5b03276292e06e0b3960c1ad99b0
isainrodriguez <isainrodriguez@localhost>
2018-02-23 16:51:50 +0100

    hechos todos las paginas de registro.

---
+++ locales.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+Pagina de locales.
+-->
+<html>
+    <head>
+        <title>OohMusic</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/estiloslogin.css">
+    </head>
+    <body>
+        <header>
+            <div class="contenedor">
+                <h1 class="icon-music">Ooh Music</h1>
+                <input type="checkbox" id="menu-bar">
+                <label class="icon-menu" for="menu-bar"></label>
+                <nav class="menu">
+                    <a href="">Inicio</a>
+                    <a href="login.php">Login</a>
+                    <a href="rmusicos.php">Registro</a>
+                    <a href="">Musicos</a>
+                    <a href="">Conciertos</a>
+                    <a href="">Fans</a>
+                    <a href="">Contacto</a>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <section id="banner">
+                <img src="Imagenes/banner.jpg">
+                
+            </section>
+            
+        </main>
+    </body>
+</html>
