b14c97742327fb6a0cb3b6154912a98716f36283
alvaro <alvaro@alvaro-linuxusb>
2018-03-06 15:11:19 +0100

    creacion de paginas Ulocal, Ufan  y Umusico que son las páginas de cada usuario logeado

---
+++ Umusico.php
@@ -0,0 +1,79 @@
+
+
+<html lang="es">
+    <head>
+        <title>OohMusic</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/estilosMusico.css">
+    </head>
+    <body>
+        <header>
+            <div class="contenedor">
+                <h1 class="icon-music">Ooh Music</h1>
+                <input type="checkbox" id="menu-bar">
+                <label class="icon-menu" for="menu-bar"></label>
+                <nav class="menu">
+                    <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li class="submenu"><a href="">Registro <span class="icon-down-dir"></span></a>
+                            <ul class="submenuu">
+                                <li><a href="rmusicos.php">Regístrate como músico</a></li>
+                                <li><a href="rlocales.php">Regístrate como local</a></li>
+                                <li><a href="rfan.php">Regístrate como fan</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="musicos.php">Musicos</a></li>
+                        <li><a href="locales.php">Locales</a></li>
+                        <li><a href="fans.php">Fans</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
+                    </ul>
+                    
+                </nav>
+            </div>
+        </header>       
+        <main>
+            <section id="banner">
+                <img src="Imagenes/banner.jpg">
+                <div id="centro"> 
+                    <p>Bienvenido</p>
+                    <div id="usuario">
+                        <p>Juan García Aguilar</p>
+                        <hr>
+                        <div id="info">
+                            <img src="Imagenes/usuario.png">
+                        </div>
+                    </div>
+                    <div id="menu">
+                        <ul>
+                            <li><a href="#">Perfil</a></li>
+                            <li><a href="#">Fotos</a></li>
+                            <li><a href="#">Mensajes</a></li>
+                            <li><a href="#">Configuración</a></li>
+                            <li><a href="#">Cerrar sesión</a></li>
+                        </ul>
+                    </div>
+                    <div id="titulonoticias">
+                        <p>Últimas noticias</p>
+                    </div>
+                    <div id="noticias">
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <div class="contenedor">
+                <p class="copy">Ooh Music &copy; 2018</p>
+                <div class="sociales">
+                    <a class="icon-facebook-squared" href="#"></a>
+                    <a class="icon-twitter" href="#"></a>
+                    <a class="icon-instagram" href="#"></a>
+                    <a class="icon-gmail" href="#"></a>
+                </div>
+            </div>
+            
+        </footer>
+    </body>
+</html>
