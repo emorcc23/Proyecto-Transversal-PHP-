462c58c081a9e0941bcc464992c31b7f4d27aa91
alvaro <alu520994162@a44o06.aulas.local>
2018-04-25 16:37:08 +0200

    Diseño autobuscador index

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
         <link rel="stylesheet" href="css/estilos.css">
         <script src="jquery.min.js"></script>
         <script type="text/javascript" src="funcionesbuscador.js"></script>
-<!--        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  -->
         <script>
             window.onload = autoCompletado();
         </script>
