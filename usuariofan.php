e75aa2ecac1407c5c632ace5c4539aede93c4238
alvaro <>
2018-05-03 15:23:26 +0200

    preparacion fase 4

--- usuariofan.php
+++ usuariofan.php
@@ -9,7 +9,7 @@ require_once 'funciones.php';
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
         <link rel="stylesheet" href="css/fontello.css">
-        <link rel="stylesheet" href="css/estilosFan.css">
+        <link rel="stylesheet" href="css/estilosMusico.css">
     </head>
     <body>
         <header>
@@ -33,63 +33,61 @@ require_once 'funciones.php';
                         <li><a href="fans.php">Fans</a></li>
                         <li><a href="contacto.php">Contacto</a></li>
                     </ul>
-                    
+
                 </nav>
             </div>
         </header>       
         <main>
             <section id="banner">
                 <?php
-                $mostrar=0;
-                if(isset($_SESSION['username']))
-                {
+                $mostrar = 0;
+                if (isset($_SESSION['username'])) {
                     //por seguridad compruebo si el usuario existe
-                    if(usuarioexiste($_SESSION['username'])>0)
-                    {
-                        $mostrar=1;
-                    }
-                    else
-                    {
+                    if (usuarioexiste($_SESSION['username']) > 0) {
+                        $mostrar = 1;
+                    } else {
                         echo"<script>alert('El usuario ya no existe')</script>";
                     }
-                }
-                else
-                {
+                } else {
                     echo"<script>alert('No puedes entrar aquí')</script>";
                 }
-                if($mostrar==1)
-                {
-                 
+                if ($mostrar == 1) {
                     ?>
                     <img src="Imagenes/banner.jpg">
-                       <div id="centro"> 
-                    <p>Datos de mi perfil</p>
-                    <div id="usuario">
-                        <?php
-                        muestradatosfan()
-                        
-                        ?>
-                    </div>
-                        
-                    <div id="menu">
-                        <ul>
-                            <li><a href="#">Perfil</a></li>
-                            <li><a href="#">Fotos</a></li>
-                            <li><a href="#">Mensajes</a></li>
-                            <li><a href="miperfilfan.php">Configuración</a></li>
-                            <?php cerraSession2()?>
-                        </ul>
-                    </div>
+                    <div id="centro"> 
+                        <p>Datos de mi perfil</p>
+                        <div id="usuario">
+                            <?php
+                            muestradatosmusico();
+                            ?>
+                        </div>
+
+                        <div id="menu">
+                            <ul>
+                                <li><a href="usuariofan.php">Perfil</a></li>
+                                <li><a href="#">Fotos</a></li>
+                                <li><a href="#">Mensajes</a></li>
+                                <li><a href="miperfilfan.php">Configuración</a></li>
+                                <?php cerraSession2() ?>
+                            </ul>
+                        </div>
+                        <div id="conciertosInteres">
+                            <p>Conciertos que pueden interesarte</p>
+                        </div>
+                        <div id="contenidoInteres">
+                            
+                        </div>
                         <div id="titulonoticias">
                             <p>Últimas noticias</p>
                         </div>
                         <div id="noticias">
+
                         </div>
                     </div>  
-                    <?php                
-                } 
+                    <?php
+                }
                 ?>
-                        
+                    
             </section>
         </main>
         <footer>
@@ -102,7 +100,7 @@ require_once 'funciones.php';
                     <a class="icon-gmail" href="#"></a>
                 </div>
             </div>
-            
+
         </footer>
     </body>
 </html>
